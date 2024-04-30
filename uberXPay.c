a1aee19f2e829a19e3f8340c105d622c4ba8e5ee
Joseph Klinck <josephklinck@Josephs-MacBook-Pro.local>
2024-04-29 23:49:39 -0700

    added user validating for all 3 user inputs

--- uberXPay.c
+++ uberXPay.c
@@ -16,25 +16,29 @@ void pay(){
 	double totalPay;
 
 	printf("\nEnter minutes: ");
-	scanf("%lf", &minutes);
-	while(minutes < 1){
-		printf("Please enter a positive number for miles: ");
-		scanf("%lf", &minutes);
+	while (scanf("%lf", &minutes) != 1 || minutes < 1){
+		printf("Invalid input!\n");
+		// clear the input buffer
+		while (getchar() != '\n');
+		printf("Enter minutes: ");
 	}
 
-	printf("Enter seconds: ");
-	scanf("%lf", &seconds);
-	while(minutes < 1){
-		printf("Please enter a positive number for miles: ");
-		scanf("%lf", &minutes);
+	printf("\nEnter seconds: ");
+	while (scanf("%lf", &seconds) != 1 || seconds < 0){
+		printf("Invalid input!\n");
+		// clear the input buffer
+		while (getchar() != '\n');
+		printf("Enter seconds: ");
 	}
 
-	printf("Enter miles: ");
-	scanf("%lf", &miles);
-	while(minutes < 1){
-		printf("Please enter a positive number for miles: ");
-		scanf("%lf", &minutes);
+	printf("\nEnter miles: ");
+	while (scanf("%lf", &miles) != 1 || miles < 1){
+		printf("Invalid input!\n");
+		// clear the input buffer
+		while (getchar() != '\n');
+		printf("Enter miles: ");
 	}
+	
 
 
 	totalPay = ((seconds / 60) + minutes) * 0.66 + miles * 1.55;
